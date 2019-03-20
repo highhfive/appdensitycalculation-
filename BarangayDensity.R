bd889338ccd0ebe3e1ed81d89a257b137e18e0a8
Migo Andres Dancel <medancel@globe.com.ph>
2019-03-20 09:18:31 +0800

    Top 5 barangay density function and csv export

--- BarangayDensity.R
+++ BarangayDensity.R
@@ -1,3 +1,4 @@
+##Calculates Area of Barangay
 region=read.csv("regionarea.csv")
 population=read.csv("population.csv")
 library(dplyr)
@@ -5,3 +6,8 @@ df1<-select(population,Region,Barangay)
 df2<-select(region,Region,Area)
 df2$count<-with(df1,table(Region))
 df2$bararea<-with(df2,Area/count)
+
+##Extracts the Top 5 Barangay with highest densities to CSV file
+orderedByDensity <- arrange(df2, desc(bararea))
+top5BarangayDensities <- slice(orderedByDensity, c(1:5))
+write.csv(top5BarangayDensities, 'top5BarangayDensities.csv')
